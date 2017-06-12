44230cc22e3a92c407554c95c1123f16ee9a8d93
往事如烟 <745861228@qq.com>
2017-06-12 22:48:06 +0800

    merage 1.0.1

--- app/src/main/java/com/like/gitprojectdemo/MainActivity.java
+++ app/src/main/java/com/like/gitprojectdemo/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button textView = new Button(this);
-        textView.setText("develop");
+        /**
+         * 合并分支
+         */
     }
 }
