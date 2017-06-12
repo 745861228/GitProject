54b7e7eda48010299534798ab2ecb885ef0f1d51
往事如烟 <745861228@qq.com>
2017-06-12 22:42:34 +0800

    1.0.1合并

--- app/src/main/java/com/like/gitprojectdemo/MainActivity.java
+++ app/src/main/java/com/like/gitprojectdemo/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button textView = new Button(this);
+        /**
+         * 合并分支
+         */
     }
 }
